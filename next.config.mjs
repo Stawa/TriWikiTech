5f80d9d1c39215b8dfbba2c08ec245af6d7e8823
Stawa <stawamusic@gmail.com>
2024-08-29 22:42:04 +0800

    Remove

--- next.config.mjs
+++ next.config.mjs
@@ -4,25 +4,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        net: false,
-        tls: false,
-      };
-    }
-    return config;
-  },
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/socket.io/:path*",
-        destination: "/api/socketio",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
