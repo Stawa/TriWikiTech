189f4e41a76f1bd29c3da1ba3b69e6fd082279a6
Stawa <stawamusic@gmail.com>
2024-09-25 16:24:27 +0800

    Trying to optimize performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { ReactNode } from "react";
 import { useLocale, useTranslations } from "next-intl";
-import { useSpring, animated } from "react-spring";
 import { FaBook, FaCode } from "react-icons/fa";
 import { IoNavigateOutline, IoNavigateSharp } from "react-icons/io5";
 
@@ -23,86 +22,16 @@ export default function Home() {
   const t = useTranslations("Home");
   const locale = useLocale() as Locale;
 
-  const titleProps = useSpring({
-    opacity: 1,
-    y: 0,
-    from: { opacity: 0, y: -20 },
-    config: { duration: 500 },
-  });
-  const subtitleProps = useSpring({
-    opacity: 1,
-    y: 0,
-    from: { opacity: 0, y: 20 },
-    config: { duration: 800, delay: 400 },
-  });
-  const sectionProps = useSpring({
-    opacity: 1,
-    y: 0,
-    from: { opacity: 0, y: 20 },
-    config: { duration: 800, easing: (t) => t },
-  });
-  const h2Props = useSpring({
-    opacity: 1,
-    y: 0,
-    from: { opacity: 0, y: -20 },
-    config: { duration: 800, easing: (t) => t },
-  });
-  const gridProps = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-    config: { duration: 500, staggerChildren: 0.1 },
-  });
-
-  const courseSpringProps = useSpring({
-    opacity: 1,
-    y: 0,
-    from: { opacity: 0, y: 20 },
-    config: { duration: 500 },
-  });
-
-  const courseImageSpringProps = useSpring({
-    scale: 1,
-    from: { scale: 1.05 },
-    config: { duration: 300 },
-  });
-
-  const courseTitleSpringProps = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-    config: { duration: 500 },
-  });
-
-  const courseDescriptionSpringProps = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-    config: { duration: 500, delay: 200 },
-  });
-
-  const courseButtonsSpringProps = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-    config: { duration: 500, delay: 400 },
-  });
-
   return (
     <Layout>
       <section className="container mx-auto px-4 py-12 md:py-24 text-center">
-        <animated.h1
-          style={titleProps}
-          className="text-4xl md:text-5xl lg:text-7xl font-extrabold mb-4 md:mb-8 bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 text-transparent bg-clip-text"
-        >
+        <h1 className="animate-text-reveal text-4xl md:text-5xl lg:text-7xl font-extrabold mb-4 md:mb-8 bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 text-transparent bg-clip-text">
           {t("title")}
-        </animated.h1>
-        <animated.p
-          style={subtitleProps}
-          className="text-lg md:text-xl lg:text-2xl text-gray-700 dark:text-gray-300 mb-8 md:mb-12 max-w-3xl mx-auto leading-relaxed"
-        >
+        </h1>
+        <p className="animate-text-reveal-from-down text-lg md:text-xl lg:text-2xl text-gray-700 dark:text-gray-300 mb-8 md:mb-12 max-w-3xl mx-auto leading-relaxed">
           {t("subtitle")}
-        </animated.p>
-        <animated.div
-          style={subtitleProps}
-          className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4 md:space-x-6"
-        >
+        </p>
+        <div className="animate-text-reveal-from-down flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4 md:space-x-6">
           <Link
             href="/#start"
             className="group relative inline-flex items-center justify-center px-6 py-3 md:px-8 md:py-4 bg-gradient-to-r from-blue-600 to-purple-600 text-white rounded-full font-bold text-base md:text-lg transition duration-300 hover:shadow-lg hover:scale-105 transform overflow-hidden"
@@ -134,29 +63,21 @@ export default function Home() {
               {t("exploreCourses")}
             </span>
           </Link>
-        </animated.div>
+        </div>
       </section>
       <section id="start" className="container mx-auto px-4 py-12 md:py-24">
-        <animated.div style={sectionProps} className="container mx-auto px-4">
-          <animated.h2
-            style={h2Props}
-            className="text-3xl md:text-4xl lg:text-5xl font-extrabold text-center mb-8 md:mb-16 bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 text-transparent bg-clip-text animate-gradient-x"
-          >
+        <div className="container mx-auto px-4">
+          <h2 className="animate-text-reveal-from-down text-3xl md:text-4xl lg:text-5xl font-extrabold text-center mb-8 md:mb-16 bg-gradient-to-r from-blue-600 via-purple-600 to-pink-600 text-transparent bg-clip-text">
             {t("exploreTools")}
-          </animated.h2>
-          <animated.div
-            style={gridProps}
-            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 lg:gap-12 md:px-4 lg:px-6"
-          >
+          </h2>
+          <div className="animate-smooth-reveal grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 lg:gap-12 md:px-4 lg:px-6">
             {Languages.map((course, index) => (
-              <animated.div
+              <div
                 key={index}
-                style={courseSpringProps}
                 className="bg-white dark:bg-gray-800 rounded-2xl overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105 flex flex-col"
               >
-                <animated.div
+                <div
                   className={`p-6 md:p-8 bg-gradient-to-br ${course.color} flex items-center justify-center w-full`}
-                  style={courseImageSpringProps}
                 >
                   <Image
                     src={course.image}
@@ -166,26 +87,17 @@ export default function Home() {
                     priority={true}
                     className="object-contain filter drop-shadow-lg w-20 h-20 md:w-28 md:h-28"
                   />
-                </animated.div>
+                </div>
                 <div className="p-6 md:p-8 flex flex-col justify-between flex-grow">
                   <div>
-                    <animated.h3
-                      style={courseTitleSpringProps}
-                      className="text-xl md:text-2xl lg:text-3xl font-bold mb-3 text-gray-800 dark:text-white"
-                    >
+                    <h3 className="text-xl md:text-2xl lg:text-3xl font-bold mb-3 text-gray-800 dark:text-white">
                       {course.title}
-                    </animated.h3>
-                    <animated.p
-                      style={courseDescriptionSpringProps}
-                      className="text-base md:text-lg text-gray-600 dark:text-gray-300 mb-6"
-                    >
+                    </h3>
+                    <p className="text-base md:text-lg text-gray-600 dark:text-gray-300 mb-6">
                       {course.description[locale]}
-                    </animated.p>
+                    </p>
                   </div>
-                  <animated.div
-                    style={courseButtonsSpringProps}
-                    className="flex flex-col space-y-3"
-                  >
+                  <div className="flex flex-col space-y-3">
                     <Link
                       href={course.link}
                       className="flex-1 inline-flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-600 hover:to-purple-700 text-white font-semibold py-3 px-4 rounded-lg transition duration-300 transform hover:scale-105 shadow-md hover:shadow-lg text-base"
@@ -200,12 +112,12 @@ export default function Home() {
                       <FaCode className="mr-3 text-lg" />
                       {t("tryCompiler")}
                     </Link>
-                  </animated.div>
+                  </div>
                 </div>
-              </animated.div>
+              </div>
             ))}
-          </animated.div>
-        </animated.div>
+          </div>
+        </div>
       </section>
     </Layout>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,26 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        reveal: {
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        revealFromDown: {
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        smoothReveal: {
+          "0%": { transform: "translateY(20px)", opacity: "0" },
+          "50%": { transform: "translateY(10px)", opacity: "0.5" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        "text-reveal": "reveal 0.8s ease-out forwards",
+        "text-reveal-from-down": "revealFromDown 0.8s ease-out forwards",
+        "smooth-reveal": "smoothReveal 0.5s ease-out forwards",
+      },
     },
   },
   plugins: [],
