54e2e7a909ac4042bcd80c1023d67a4efd7b78c6
Stawa <stawamusic@gmail.com>
2024-09-26 21:00:09 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,15 @@ export default async function RootLayout({
             gtag('config', 'G-XYL68P0QK4');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "o99d4baayk");
+          `}
+        </Script>
       </head>
       <body className={`${arimo.className} antialiased`}>
         <NextIntlClientProvider messages={messages}>
