19af4b966b1e9be4ea1be190f23ba95d605d93e0
Stawa <stawamusic@gmail.com>
2024-10-21 09:56:52 +0800

    Try

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,8 @@ export default defineConfig(({ mode }) => {
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
           v3_throwAbortReason: true,
+          v3_singleFetch: true,
+          v3_lazyRouteDiscovery: true,
         },
       }),
       tsconfigPaths(),
